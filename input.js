dafb8690c193bb4a0faac9d7a59652156ea795b9
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-23 18:21:57 +0000

    binding controls wasd

--- input.js
+++ input.js
@@ -1,15 +1,27 @@
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
+let connection;
 const handleUserInput = function(key) {
-  if(key === '\u0003') {
-    process.exit();
-  }
-};
+  if(key === '\u0003')
+    process.exit();  
+  else if (key === 'w') {
+    connection.write("Move: up")
+} else if (key === 'a') {
+    connection.write("Move: left")
+} else if (key === 's') {
+    connection.write("Move: down")
+} else if (key === 'd') {
+    connection.write("Move: right")
+} else if (key === '1') {
+    connection.write("Say: Sucka")
+}
+}
+const setupInput = function(conn) {
+    connection = conn;
+    const stdin = process.stdin;
+      stdin.setRawMode(true);
+      stdin.setEncoding('utf8');
+      stdin.resume();
+      stdin.on('data',handleUserInput);
+      return stdin;    
+    }
 
-module.exports = { setupInput };
+module.exports = { setupInput }
