2234b0afb9cd481866576e0b136f764cfaca2d80
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-23 18:04:32 +0000

    moved handleUserInput and setupInput to a new file input.js and exported to play.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 // play.js
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if(key === '\u0003') {
-    process.exit();
-  }
-};
-
-
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
+
+setupInput();
