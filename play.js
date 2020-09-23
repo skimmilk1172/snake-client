9180e0535e14c76eaef2e3dcfc3c823bf6f77634
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-23 18:00:18 +0000

    Added function to exit

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
 // play.js
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if(key === '\u0003') {
+    process.exit();
+  }
+};
+
+
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
