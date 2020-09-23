0d445cb271d218035ae800f95ae326fc43301372
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-23 17:22:11 +0000

    handle message

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(`Server: ${data}`);
+  });
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
